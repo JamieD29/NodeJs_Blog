55f396667a0097e8b387b2d3b9080f59dff534b4
JamieD <mtriet193@gmail.com>
2023-05-12 16:02:14 +0700

    Test husky

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-const path = require('node:path');
-const express = require('express');
-const morgan = require('morgan');
-const handlebars = require('express-handlebars');
-const router = require('./resource/routes/index.route');
+const path = require("node:path");
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+const router = require("./resource/routes/index.route");
 const app = express();
 const port = 3002;
 
-app.use(express.static(path.join(__dirname, 'public')));
-                app.use(express.urlencoded({ extended: 'true' }));
-      app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: "true" }));
+app.use(express.json());
 
 //HTTP logger
-    app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 //Template engine
- app.engine(
-  '.hbs',
+app.engine(
+  ".hbs",
   handlebars.engine({
-    extname: '.hbs',
-  }),
+    extname: ".hbs",
+  })
 );
-           app.set('view engine', '.hbs');
-    app.set('views', path.join(__dirname, 'resource', 'views'));
+app.set("view engine", ".hbs");
+app.set("views", path.join(__dirname, "resource", "views"));
 
 router(app);
 
