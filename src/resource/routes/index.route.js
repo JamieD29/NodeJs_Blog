2e234094e91f31ab3c198b405e0e2091e5a0eb63
JamieD <mtriet193@gmail.com>
2023-05-12 15:47:59 +0700

    Install prettier, lint-staged, husky

--- src/resource/routes/index.route.js
+++ src/resource/routes/index.route.js
@@ -1,22 +1,22 @@
-const newsRouter = require("./news.route");
-const siteRouter = require('./site.route')
+const newsRouter = require('./news.route');
+const siteRouter = require('./site.route');
 
 const route = (app) => {
-//   app.get("/", (req, res) => {
-//     res.render("home");
-//   });
+  //   app.get("/", (req, res) => {
+  //     res.render("home");
+  //   });
 
   // app.get("/news", (req, res) => {
   //   res.render("news");
   // });
 
-  app.use("/news", newsRouter);
+  app.use('/news', newsRouter);
 
-  app.use('/', )
+  app.use('/');
 
-//   app.get("/search", (req, res) => {
-//     res.render("search");
-//   });
+  //   app.get("/search", (req, res) => {
+  //     res.render("search");
+  //   });
 
   //   app.post("/search", (req, res) => {
   //     console.log(req.body);
--- src/resource/routes/news.route.js
+++ src/resource/routes/news.route.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const newController = require("../app/controllers/NewsController");
+const newController = require('../app/controllers/NewsController');
 
-router.use("/:slug", newController.show);
+router.use('/:slug', newController.show);
 router.use('/', newController.index);
 
 module.exports = router;
