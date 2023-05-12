8ae1b71c2a479dbc1c8adaa5480510af0d0d4718
JamieD <mtriet193@gmail.com>
2023-05-12 15:56:26 +0700

    test

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
     extname: '.hbs',
   }),
 );
-                       app.set('view engine', '.hbs');
+                  app.set('view engine', '.hbs');
     app.set('views', path.join(__dirname, 'resource', 'views'));
 
 router(app);
